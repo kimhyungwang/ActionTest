9c5bac9686e47ed908fb475fe8611655ef34eafe
hyungwang <kfk10112@gmail.com>
2024-06-17 15:05:18 +0900

    Commit message

--- src/main/java/com/nhnacademy/actiontest/HomeController.java
+++ src/main/java/com/nhnacademy/actiontest/HomeController.java
@@ -1,4 +1,16 @@
 package com.nhnacademy.actiontest;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HomeController {
+	@Value("${app.version:1.0.0}")
+	String version;
+
+	@GetMapping("/")
+	public String home() {
+		return "Hello CICD! version: " + version;
+	}
 }

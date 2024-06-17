9264b2e21d1b83b1c9afacbeb6b8caed14b6a323
hyungwang <kfk10112@gmail.com>
2024-06-17 15:03:50 +0900

    1

---
+++ src/main/java/com/nhnacademy/actiontest/HomeController.java
@@ -0,0 +1,4 @@
+package com.nhnacademy.actiontest;
+
+public class HomeController {
+}
---
+++ src/test/java/com/nhnacademy/actiontest/ActionTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.actiontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
